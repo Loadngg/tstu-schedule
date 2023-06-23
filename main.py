1076196e63253e92b1ee616df15d47c62d6a4677
Ярослав Аверин <averinj@mail.ru>
2023-06-23 11:05:28 +0300

    > fix #3 (1) > some ui/ux changes

--- main.py
+++ main.py
@@ -14,31 +14,32 @@ class Application(customtkinter.CTk):
 
         self.parser = Parser()
 
-        self.title("Парсинг заочников")
+        self.title("Парсинг расписания")
         self.geometry(f"{500}x{250}")
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
-        self.search_frame = customtkinter.CTkFrame(self, width=140, height=50, corner_radius=0)
+        self.search_frame = customtkinter.CTkFrame(self, width=140, height=70, corner_radius=0)
         self.search_frame.grid(row=0, column=0, sticky="nsew")
         self.search_frame.grid_columnconfigure(1, weight=1)
 
         self.search_label = customtkinter.CTkLabel(self.search_frame, text="Поиск по:", anchor="e")
         self.search_label.grid(row=0, column=0, padx=20, pady=(10, 0))
 
-        self.search_filter = customtkinter.CTkTextbox(self.search_frame, width=20, height=20, wrap="word")
-        self.search_filter.grid(row=0, column=1, padx=(20, 10), pady=20, sticky="ew")
+        self.search_filter = customtkinter.CTkTextbox(self.search_frame, width=20, height=40, wrap="none")
+        self.search_filter.grid(row=0, column=1, padx=(0, 20), pady=20, sticky="ew")
+        self.search_filter.bind('<Return>', lambda event: 'break')
+        self.search_filter.configure(font=('', 16))
 
-        self.buttons_frame = customtkinter.CTkFrame(self, width=140, height=100, corner_radius=0)
-        self.buttons_frame.grid(row=1, column=0, sticky="nsew")
-        self.buttons_frame.grid_columnconfigure(0, weight=1)
+        self.general_file_checkbox = customtkinter.CTkCheckBox(self, text="Вывод в общий файл")
+        self.general_file_checkbox.grid(row=1, column=0, padx=20, pady=10, sticky="nsew")
 
-        self.load_button = customtkinter.CTkButton(self.buttons_frame, text="Загрузить", command=self.load_button_event)
-        self.load_button.grid(row=0, column=0, padx=20, pady=10, sticky="nsew")
+        self.load_button = customtkinter.CTkButton(self, text="Загрузить", command=self.load_button_event)
+        self.load_button.grid(row=2, column=0, padx=20, pady=10, sticky="nsew")
 
-        self.search_button = customtkinter.CTkButton(self.buttons_frame, text="Поиск", command=self.parse_button_event,
+        self.search_button = customtkinter.CTkButton(self, text="Поиск", command=self.parse_button_event,
                                                      state="disabled")
-        self.search_button.grid(row=1, column=0, padx=20, pady=10, sticky="nsew")
+        self.search_button.grid(row=3, column=0, padx=20, pady=10, sticky="nsew")
 
     def load_button_event(self) -> None:
         files = fd.askopenfilenames()
